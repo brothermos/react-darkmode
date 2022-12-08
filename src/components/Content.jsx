7b87df6b14a98093b3af02eadbc4c18018f6a506
Natdanai Kanyakoon <moss10612@gmail.com>
2022-12-08 15:00:02 +0700

    first commite

---
+++ src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import light from "../assets/image/light.svg";
+import dark from "../assets/image/dark.svg";
+import { ThemeContext } from "../App";
+const Content = () => {
+   const { theme } = useContext(ThemeContext);
+   return (
+      <main className={theme === "dark" ? "dark" : "light"}>
+         <div>
+            <h1>Natdanai Kanyakoon</h1>
+            <p>DarkMode Workshop</p>
+         </div>
+         <img src={theme === "dark" ? dark : light} alt="Logo" />
+      </main>
+   );
+};
+
+export default Content;
